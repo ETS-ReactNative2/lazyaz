383f0bdff492ebc3054fc5ab21fe6185facd6812
lemilany <legaspi.emilany@gmail.com>
2018-08-27 09:27:05 +1200

    36219464: added splash screen

--- app/index.js
+++ app/index.js
@@ -1,1 +1,25 @@
-export default () => (null);
+import React, { Component } from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import SplashScreen from 'react-native-splash-screen';
+
+import LoginSignup from './screens/LoginSignup';
+
+EStyleSheet.build({
+  $primaryYellow: '#FECF33',
+  $primaryLightOrange: '#FDBD39',
+  $primaryPeach: '#EE6723',
+});
+
+class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return (
+      <LoginSignup />
+    );
+  }
+}
+
+export default App;
